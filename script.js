012fd957c5845467f8c743c13300fa4161f04a09
Geetika-08 <geetikachopra468@gmail.com>
2024-07-22 20:19:39 +0530

    Update script.js
    changed script

--- script.js
+++ script.js
@@ -6,25 +6,8 @@ function updateOutput(){
     const output = document.getElementById('output');
 
     output.contentDocument.body.innerHTML = htmlCode.value + "<style>" + cssCode.value+ "</style>";
-        //output refers to the iframe element.
-        // contentDocument is a property of the iframe that provides access to the document within the iframe.
-        // body refers to the <body> element of the document inside the iframe.
-        // innerHTML is a property that allows you to set or get the HTML content inside an element.
-
-        //htmlCode.value + "<style>" + cssCode.value + "</style>" combines the HTML code written by the user with the CSS code wrapped in a <style> tag.
-        //"<style>" and "</style>" are HTML tags used to define a block of CSS.
-
     output.contentWindow.eval(jsCode.value);
-        //eval is a JavaScript function that evaluates or executes a string of JavaScript code.
-        //contentWindow is a property of the iframe that provides access to the window object of the document inside the iframe
 }
-
-
-
-// example:
-
-// document.body.style.backgroundColor = 'lightblue';
-// alert('Hello from the iframe!');
 
 
 
